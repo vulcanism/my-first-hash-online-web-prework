656b1217d4599b04be3fd477bf57f39220850c7b
Savannah C <sgcorrell@gmail.com>
2019-03-05 19:59:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,10 +14,6 @@ def shipping_manifest
               "oil paintings" => 3
   }
   
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
 
 end
 
@@ -28,7 +24,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
